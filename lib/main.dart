0be64ff11571f33454a956332a89c6c50846353c
Ankit Subedi <ankitsubedi89@gmail.com>
2022-08-01 15:52:36 +0545

    [FEAT] red appBar

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // "hot reload" (press "r" in the console where youaa ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
@@ -74,6 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        backgroundColor: Colors.red,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
